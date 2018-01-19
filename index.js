ecaabe743ffc85a03942b94ff903235cde94cf8a
Tim Piotrowski <timpio95@web.de>
2018-01-19 20:43:41 +0100

    Finished Version

--- index.js
+++ index.js
@@ -39,25 +39,62 @@ var loader = new THREE.ObjectLoader();
   // then directly add the object
   scene.add(object);
 });*/
-loader.load('assets/sg-sword-13.json', function(object) {
 
-  // if you want to add your custom material
-  var materialObj = new THREE.MeshBasicMaterial({
-    vertexColors: THREE.FaceColors,
-    overdraw: 0.5
-  });
-  console.log('materialObj', materialObj);
-  object.traverse(function(child) {
-    if (child instanceof THREE.Mesh) {
-      child.material = materialObj;
+AFRAME.registerComponent('model-opacity', {
+    console.log("Test");
+    schema: {default: 1.0},
+    init: function () {
+        this.el.addEventListener('model-loaded', this.update.bind(this));
+    },
+    update: function () {
+        var mesh = this.el.getObject3D('mesh');
+        var data = this.data;
+        if (!mesh) { return; }
+        mesh.traverse(function (node) {
+            if (node.isMesh) {
+                node.material.opacity = data;
+                node.material.transparent = data < 1.0;
+                node.material.needsUpdate = true;
+            }
+        });
     }
-  });
-  object.scale.set(0.01, 0.01, 0.01);
-  object.position.x = 2;
-  object.position.y = 1;
-  object.position.z = -4;
+});
 
+AFRAME.registerComponent('pivot', {
+    dependencies: ['position'],
 
-  // then directly add the object
-  scene.add(object);
+    schema: {type: 'vec3'},
+
+    init: function () {
+        var data = this.data;
+        var el = this.el;
+        var originalParent = el.object3D.parent;
+        var originalGroup = el.object3D;
+        var outerGroup = new THREE.Group();
+
+        originalPosition.copy(originalGroup.position);
+        originalRotation.copy(originalGroup.rotation);
+
+        // Detach current group from parent.
+        originalParent.remove(originalGroup);
+        outerGroup.add(originalGroup);
+
+        // Set new group as the outer group.
+        originalParent.add(outerGroup);
+
+        // Set outer group as new object3D.
+        el.object3D = outerGroup;
+
+        // Apply pivot to original group.
+        originalGroup.position.set(-1 * data.x, -1 * data.y, -1 * data.z);
+
+        // Offset the pivot so that world position not affected.
+        // And restore position onto outer group.
+        outerGroup.position.set(data.x + originalPosition.x, data.y + originalPosition.y,
+            data.z + originalPosition.z);
+
+        // Transfer rotation to outer group.
+        outerGroup.rotation.copy(originalGroup.rotation);
+        originalGroup.rotation.set(0, 0, 0);
+    }
 });
